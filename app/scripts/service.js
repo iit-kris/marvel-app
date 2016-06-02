b307a63fed8e4fdde60bea3d348fd5c44b7b08aa
Kristoffer Albeus <kristoffer.albeus@industrieit.com>
2016-06-02 15:42:45 +1000

    Refactor query parameters in service.js.

--- app/scripts/service.js
+++ app/scripts/service.js
@@ -1,5 +1,10 @@
 marvelApp.service('CharacterService', function($http) {
+   this.standardParams = {
+       apikey: '01f17a8c218b6ddb271b3457769cb674',
+       ts: '00',
+       hash: '579ee81a9d0c7ce3e3fc5ec42dfec338'
+   },
    this.fetchCharacters = function() {
-     return $http.get('http://gateway.marvel.com:80/v1/public/characters?apikey=01f17a8c218b6ddb271b3457769cb674&ts=00&hash=579ee81a9d0c7ce3e3fc5ec42dfec338');
+     return $http.get('http://gateway.marvel.com:80/v1/public/characters', {params: this.standardParams});
    };
 });
