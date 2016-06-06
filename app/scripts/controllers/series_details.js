781c9fa1ae6cf3627ff33c0e09bb0cc9890dffef
Kristoffer Albeus <kristoffer.albeus@industrieit.com>
2016-06-06 17:28:08 +1000

    Add "Load More..." functionality.

--- app/scripts/controllers/series_details.js
+++ app/scripts/controllers/series_details.js
@@ -1,7 +1,7 @@
 marvelApp.controller('SeriesDetailsController', function($scope, $routeParams, SeriesService) {
     $scope.seriesDetails = $scope.seriesDetails || {};
     $scope.seriesId = $routeParams.seriesId;
-    $scope.initSeriesDetails = function() {
+    $scope.loadSeriesDetails = function() {
         SeriesService.fetchSeriesDetails($scope.seriesId).then(function(data) {
             if(data.status === 200) {
                 console.log(data.data.data.results[0]);
@@ -12,5 +12,5 @@ marvelApp.controller('SeriesDetailsController', function($scope, $routeParams, S
         });
     };
 
-    $scope.initSeriesDetails();
+    $scope.loadSeriesDetails();
 });
