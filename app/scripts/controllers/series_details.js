0814e4e0ad7dfed3e6bfd0bf599957b7375e4f2b
Kristoffer Albeus <kristoffer.albeus@industrieit.com>
2016-06-06 12:24:24 +1000

    Add series details.

---
+++ app/scripts/controllers/series_details.js
@@ -0,0 +1,16 @@
+marvelApp.controller('SeriesDetailsController', function($scope, $routeParams, SeriesService) {
+    $scope.seriesDetails = $scope.seriesDetails || {};
+    $scope.seriesId = $routeParams.seriesId;
+    $scope.initSeriesDetails = function() {
+        SeriesService.fetchSeriesDetails($scope.seriesId).then(function(data) {
+            if(data.status === 200) {
+                console.log(data.data.data.results[0]);
+                $scope.series = data.data.data.results[0];
+            }
+        }, function(error) {
+           console.log(error);
+        });
+    };
+
+    $scope.initSeriesDetails();
+});
